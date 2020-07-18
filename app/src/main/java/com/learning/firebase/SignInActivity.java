de90fb49af7f598bf958b713d620df477d000b03
ravinsingh jain <ravjain@adobe.com>
2020-07-17 23:49:49 -0700

    Removing code for master branch

--- app/src/main/java/com/learning/firebase/SignInActivity.java
+++ app/src/main/java/com/learning/firebase/SignInActivity.java
@@ -47,7 +47,7 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
     private GoogleSignInClient mSignInClient;
 
     // TODO Step 4. - Add the firebase auth variable
-    private FirebaseAuth mFirebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,67 +68,22 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         mSignInClient = GoogleSignIn.getClient(this, gso);
 
         // TODO Step 5. - Initialize FirebaseAuth
-        mFirebaseAuth = FirebaseAuth.getInstance();
+
     }
 
 
     @Override
     public void onClick(View v) {
         // TODO Step 6. - Add onclick for sign in with google
-        switch (v.getId()) {
-            case R.id.sign_in_button:
-                signIn();
-                break;
-        }
+
     }
 
     // TODO Step 7. - Add Sign in method to open google sign in page
-    private void signIn() {
-        Intent signInIntent = mSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
+
 
     // TODO Step 8. - Add onActivityResult for the result of sign in
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
 
-        // Result returned from launching the Intent in signIn()
-        if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                // Google Sign In was successful, authenticate with Firebase
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                firebaseAuthWithGoogle(account);
-            } catch (ApiException e) {
-                // Google Sign In failed, update UI appropriately
-                Log.w(TAG, "Google sign in failed", e);
-            }
-        }
-    }
 
     // TODO Step 9. - Add the firebaseAuthWithGoogle method to authenticate based on signed in user
-    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
-        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
-        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
-        mFirebaseAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
 
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-                        if (!task.isSuccessful()) {
-                            Log.w(TAG, "signInWithCredential", task.getException());
-                            Toast.makeText(SignInActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        } else {
-                            startActivity(new Intent(SignInActivity.this, MainActivity.class));
-                            finish();
-                        }
-                    }
-                });
-    }
 }
